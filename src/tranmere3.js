f8ec9b2c5e75833a6b3de21cf1b8f9df1d648622
chrisowen <chrisowen491@gmail.com>
2021-02-18 22:55:27 +0000

    workflow update

--- src/tranmere3.js
+++ src/tranmere3.js
@@ -2,7 +2,7 @@ const AWSXRay = require('aws-xray-sdk');
 
 exports.handler = async function (event, context) {
   return {
-    "headers": { 'Content-Type': 'application/json'},
+    "headers": { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
     "statusCode": 200,
     "body": JSON.stringify({message: 'hello world'})
   };
