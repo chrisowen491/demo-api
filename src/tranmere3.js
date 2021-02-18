088a3ad7c9dfa118ace740b417988dc6a052c77b
chrisowen <chrisowen491@gmail.com>
2021-02-18 21:48:07 +0000

    first commit

---
+++ src/tranmere3.js
@@ -0,0 +1,9 @@
+const AWSXRay = require('aws-xray-sdk');
+
+exports.handler = async function (event, context) {
+  return {
+    "headers": { 'Content-Type': 'application/json'},
+    "statusCode": 200,
+    "body": JSON.stringify({message: 'hello world'})
+  };
+}
